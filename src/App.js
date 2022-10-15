f5ec3d71fbbc2e47bcbec5c37333e4768d66680c
Younous Saleh <younisssaleh10@gmail.com>
2022-10-15 15:59:33 -0400

    fix some bugs

--- src/App.js
+++ src/App.js
@@ -1,38 +1,58 @@
-import './App.css';
-import React from 'react';
-import {Routes,Route,Link} from 'react-router-dom';
-import {Layout,Typography,Space} from 'antd';
-import {Navbar,Homepage,Cryptocurrencies,CryptoDetails,News} from "./components"
+import "./App.css";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
 function App() {
-  
   return (
     <div className="app">
-
-      <div className='navbar'>
-        <Navbar/>
+      <div className="navbar">
+        <Navbar />
       </div>
-      <div className='main'>
+      <div className="main">
         <Layout>
-          <div className='routes'> 
+          <div className="routes">
             <Routes>
-              <Route exact path='/' element={<Homepage/>}/>
-              <Route exact path='/cryptocurrencies' element={<Cryptocurrencies/>}/>
-              <Route exact path='/crypto/:coinId' element={<CryptoDetails/>}/>
-              <Route exact path='/news' element={<News/>}/>
+              <Route exact path="/" element={<Homepage />} />
+              <Route
+                exact
+                path="/cryptohex-master"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
             </Routes>
           </div>
         </Layout>
-      <div className='footer'>
-        <Typography.Title level={5} style={{color:'white', textAlign:'center'}}>
-            CryptoHex<br/>
-            by 🚀 <a href='https://github.com/Younis1-png' target="_blank" rel="noreferrer" style={{color:"#A5C9CA"}}>Younous</a>
-        </Typography.Title>
-        <Space>
-          <Link to='/'>Home</Link>
-          <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
-          <Link to='/news'>News</Link>
-        </Space>
-      </div>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            CryptoHex
+            <br />
+            by 🚀{" "}
+            <a
+              href="https://github.com/Younis1-png"
+              target="_blank"
+              rel="noreferrer"
+              style={{ color: "#A5C9CA" }}
+            >
+              Younous
+            </a>
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
     </div>
   );
